a5f8518409bd07c07bb0e83209831adc27318386
HAMUZA <kiggunduhamuza16@gmail.com>
2024-07-30 02:57:19 -0700

    counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[count,setCount] = useState(0)
+   
+  const handleCount = () => {
+    setCount(count + 1)
+  }
+    
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>{count}</p>
+        <button onClick={handleCount}>Click here</button>
     </div>
   )
 }
