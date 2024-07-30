8cbdf4ec90bbe29c6eb714f1074e8ab26256ec4c
HAMUZA <kiggunduhamuza16@gmail.com>
2024-07-30 03:01:53 -0700

    buttoncount

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,11 @@ import './../styles/App.css';
 const App = () => {
   const[count,setCount] = useState(0)
    
-  const handleCount = () => {
-    setCount(count + 1)
-  }
-    
   return (
     <div>
         {/* Do not remove the main div */}
         <p>{count}</p>
-        <button onClick={handleCount}>Click here</button>
+        <button onClick={() => setCount(count+1)}>Click me</button>
     </div>
   )
 }
